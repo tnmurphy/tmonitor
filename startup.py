0207cdbb2463b072a75fe1af69353ddc1f4ad71c
Timothy Murphy <tnmurphy@gmail.com>
2025-11-07 07:30:35 +0000

    slight reorg

--- startup.py
+++ startup.py
@@ -1,9 +1,8 @@
 import uvicorn
-from fastapi import FastAPI
 import database
 
 if __name__ == "__main__":
     import main
     database.add_engine_to_app(main.app)
 
-    uvicorn.run("main:app", port=5000, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")
