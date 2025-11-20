fc7f7508e61764c01e38910a5bc62fabe66c37c3
Timothy Murphy <tnmurphy@gmail.com>
2025-11-20 14:09:12 +0000

    build to run in /tmonitor don't attempt to serve the app from fastapi

--- main.py
+++ main.py
@@ -71,23 +71,6 @@ app.add_middleware(
 )
 
 
-class SPAStaticFiles(StaticFiles):
-    """
-    Support client-side routing in Single Page Applications (SPAs).
-    Override the get_response method, which is responsible for retrieving
-    static file responses for given paths.
-    """
-
-    async def get_response(self, path: str, scope):
-        try:
-            return await super().get_response(path, scope)
-        except (HTTPException, StarletteHTTPException) as ex:
-            if ex.status_code == 404:
-                return await super().get_response("index.html", scope)
-            else:
-                raise ex
-
-
 @app.middleware("http")
 async def add_logger_with_correlator(request: Request, call_next):
     """
@@ -136,14 +119,6 @@ def generic_exception_handler(request: Request, exc: Exception):
         status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
         headers={"X-Correlation-Id": request.state.correlator},
     )
-
-
-# Mount the SPAStaticFiles instance at the root URL ("/").
-# This means any request not caught by other routes (like the API) will be handled here.
-# - 'directory' specifies where your built static files (from Next.js) reside.
-# - 'html=True' tells FastAPI to serve HTML files by default.
-# - 'name' is an identifier for this mounted application
-app.mount("/app", SPAStaticFiles(directory="app/temperature-chart/build", html=True), name="monitor_app")
 
 @app.post("/sense", response_class=JSONResponse)
 def sensor_event(request: Request, readings: List[SensorReadingPayload]):
