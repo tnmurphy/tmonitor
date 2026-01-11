e807c2a6ef70f906b9cd736d44d383bd31ec5ed4
Timothy Murphy <tnmurphy@gmail.com>
2026-01-11 23:37:23 +0000

    The new scheme works without crashing.

--- app/temperature-chart/src/App.js
+++ app/temperature-chart/src/App.js
@@ -37,32 +37,22 @@ class ViewStyle {
 
 
 class View {
-  constructor(viewStyle, startTime) {
+  constructor(viewStyle, startTimestamp=0, endTimestamp=1) {
     // The initial timestamp and end timestamp if no start timestamp is specified
-    // const now = new Date();
     this.viewStyle = viewStyle;
-    [this.startTimestamp, this.endTimestamp] = this.calcTimeRange(startTime);
+    this.startTimestamp = startTimestamp;
+    this.endTimestamp = endTimestamp;
   }
 
-  calcTimeRange(now) {
-    const adjusted = new Date();
-  
-    // Subtract the specified period
-    switch (this.period.periodName.toLowerCase()) {
-      case 'hour':
-        adjusted.setHours(now.getHours() - 1);
-        break;
-      case 'day':
-        adjusted.setDate(now.getDate() - 1);
-        break;
-      case 'week':
-        adjusted.setDate(now.getDate() - 7);
-        break;
-      default:
-        break;
-    }
-  
-    switch (this.period.periodName.toLowerCase()) {
+  static atNow(style) {
+    const now = new Date();
+    const view = new View(style);
+    view.timeRangeEndingNow(now);
+    return view;
+  }
+
+  snapToNearest(adjusted) { 
+    switch (this.viewStyle.periodName.toLowerCase()) {
       case 'hour':
         // Set minutes, seconds, and milliseconds to 0 to get the start of the current hour
         adjusted.setMinutes(0, 0, 0);
@@ -77,32 +67,83 @@ class View {
       default:
         throw new Error("Invalid period. Use 'hour', 'day', or 'week'.");
     }
+  }
+
+  moveBackOneView(adjusted) { 
+    // Subtract the specified period
+    switch (this.viewStyle.periodName.toLowerCase()) {
+      case 'hour':
+        adjusted.setHours(adjusted.getHours() - 1);
+        break;
+      case 'day':
+        adjusted.setDate(adjusted.getDate() - 1);
+        break;
+      case 'week':
+        adjusted.setDate(adjusted.getDate() - 7);
+        break;
+      default:
+        break;
+    }
+  }; 
+
+  timeRangeEndingNow(now) {
+    const adjusted = new Date(now);
+    this.moveBackOneView(adjusted);
+    this.snapToNearest(adjusted);
 
     // Return the timestamp in seconds
-    console.log(" Selecting start time for graph. Current time is: " + now + " adjusted to be one " + this.period.periodName +" back: " + adjusted);
-    return [Math.floor(adjusted.getTime()/1000), Math.floor(now.getTime()/1000)];
+    this.startTimestamp = Math.floor(adjusted.getTime()/1000);
+    this.endTimestamp = Math.floor(now.getTime()/1000);
+    console.log("timeRangeEndingNow: view: " +this.viewStyle.periodName + " end-start = " + (this.endTimestamp - this.startTimestamp));
   };
 
   getBucketCount() {
-     Math.max(1,Math.round((this.endTimestamp - this.startTimestamp)/this.viewStyle.movePeriod)) // One point per hour
-  }
+     return Math.max(1,Math.round((this.endTimestamp - this.startTimestamp)/this.viewStyle.movePeriod)); // One point per hour
+  };
+
+  static moveBackwards(previous) {
+    console.log("View.moveBackwards: "+previous.viewStyle.periodName);
+    const nextView = new View(previous.viewStyle);
+    nextView.endTimestamp = Math.max(previous.endTimestamp - previous.viewStyle.movePeriod, 0);
+    nextView.startTimestamp = Math.max(previous.startTimestamp - previous.viewStyle.movePeriod, 0);
+    console.log("backwards: "+nextView.viewStyle.periodName);
+    return nextView;
+  };
+
+  static moveForwards(previous) {
+    const nextView = new View(previous.viewStyle);
+    const nowStamp = Math.floor(Date.now() / 1000.0);
+
+    // Dont move into the future.
+    console.log("MoveForwards: nowStamp=" + nowStamp);
+    const nextEndTimestamp = Math.min(previous.endTimestamp + previous.viewStyle.movePeriod, nowStamp);
+    const movement = nextEndTimestamp - previous.endTimestamp
+
+    nextView.endTimestamp = nextEndTimestamp;
+    nextView.startTimestamp = previous.startTimestamp + movement;
+    console.log("MoveForwards: " + nextView.viewStyle.periodName +" "+nextView.startTimestamp);
+    // A shallow copy to ensure react sees a change.
+    return nextView;
+  };
+
 }
 
 const hourView = new ViewStyle(
   "hour",
   3600, // 1 hour in seconds
-  300, // 5 minutes in seconds
+  1800, // 30 minutes in seconds
   3600, // 1 hour in seconds
   (ts) => moment(ts * 1000).format("mm"), // Show minutes
   (ts) => moment(ts * 1000).format("HH:mm"), // Show minutes
+  (startTimestamp, endTimestamp) => Math.max(1,Math.round((endTimestamp - startTimestamp)/300)), // One point per 5 minutes
   "avg" // Only average for hour view
 );
 
 const dayView = new ViewStyle(
   "day",
   86400, // 1 day in seconds
-  3600, // 1 hour in seconds
-  86400, // 1 day in seconds
+  3600,  // 1 hour in seconds
+  86400/2, // 1/2 day in seconds
   (ts) => moment(ts * 1000).format("HH:mm"), // Show hours and minutes
   (ts) => moment(ts * 1000).format("HH:mm Do"), // Show day and hour at the edges of the axis
   (startTimestamp, endTimestamp) => Math.max(1,Math.round((endTimestamp - startTimestamp)/3600)), // One point per hour
@@ -119,21 +160,21 @@ const weekView = new ViewStyle(
   (startTimestamp, endTimestamp) => Math.max(1,Math.round((endTimestamp - startTimestamp)/86400)), // One point per day
   "min,max,avg" // Min, max and average for week view
 );
-
+const initialView = View.atNow(hourView);
 
 function App() {
 
+ const [currentView, setCurrentView] = useState(initialView);
   const [data, setData] = useState([]);
-  const [currentView, setCurrentView] = useState(new View(hourView, new Date.now()));
   const [sensors, setSensors] = useState([]);
   const [selectedSensor, setSelectedSensor] = useState(null);
 
   let CustomizedAxisTick = ({ x, y, payload, index, data }) => {
     let tick = "";
     if (index === 0 || index === data.length - 2) {
-      tick = currentView.edgeFormatter(payload.value);
+      tick = currentView.viewStyle.edgeFormatter(payload.value);
     } else {
-      tick = currentView.tickFormatter(payload.value);
+      tick = currentView.viewStyle.tickFormatter(payload.value);
     }
     return (
       <g transform={`translate(${x},${y})`}>
@@ -196,9 +237,15 @@ function App() {
 
   useEffect(() => {
     if (!selectedSensor) return;
-
+    console.log("fetching view: " + currentView.viewStyle.periodName);
+    console.log("view keys: " + Object.keys(currentView));
+    console.log("view methods:" + Object.getOwnPropertyNames(currentView));
+    console.log("Buckets: " + currentView.getBucketCount())
+    console.log("endTimestamp: " + currentView.endTimestamp);
+    console.log("startTimestamp: " + currentView.startTimestamp);
+    const period = currentView.endTimestamp - currentView.startTimestamp;
     fetch(
-`${API_URL}/sample?sensors=${selectedSensor}&start_timestamp=${currentView.startTimestamp}&period=${currentView.period}&units=C&sample_buckets=${currentView.getBucketCount()}&sample_methods=${currentView.currentViewStyle.sampleMethods}&limit=3000`,
+`${API_URL}/sample?sensors=${selectedSensor}&start_timestamp=${currentView.startTimestamp}&period=${period}&units=C&sample_buckets=${currentView.getBucketCount()}&sample_methods=${currentView.viewStyle.sampleMethods}&limit=3000`,
       { mode: "cors" }
     )
       .then((response) => response.json())
@@ -211,23 +258,34 @@ function App() {
       .catch((error) => console.error("Error fetching data:", error));
   }, [currentView, selectedSensor]);
 
-  const moveBackward = () => {
-    newView = new View(currentView.viewStyle)
-    setCurrentView(newView)
-    setStartTimestamp((prev) => Math.max(prev - currentView.movePeriod, 0));
+  const moveBackwards = () => {
+    console.log("moveBackwards: "+currentView.viewStyle.periodName);
+    const newView = View.moveBackwards(currentView);
+    setCurrentView(newView);
   };
 
-  const moveForward = () => {
-    setStartTimestamp((prev) => prev + currentView.movePeriod);
+  const moveForwards = () => {
+    const newView = View.moveForwards(currentView);
+    setCurrentView(newView);
   };
 
-  const moveFastBackward = () => {
-    setStartTimestamp((prev) => Math.max(prev - currentView.fastMovePeriod, 0));
+  const moveFastBackwards = () => {
+    const newView = View.moveFastBackwards(currentView);
+    setCurrentView(newView);
   };
 
-  const moveFastForward = () => {
-    setStartTimestamp((prev) => prev + currentView.fastMovePeriod);
+  const moveFastForwards = () => {
+    const newView = View.moveFastForwards(currentView);
+    setCurrentView(newView);
   };
+
+  const setViewStyle = (style) => {
+    const newView = new View(style);
+    Object.assign(newView, currentView);
+    newView.viewStyle =	style;
+    setCurrentView(newView);
+  }
+ 
 
 
 
@@ -236,7 +294,8 @@ function App() {
 
 
   const setToNow = () => { 
-      setStartTimestamp(getAdjustedTimestamp(currentView))
+      const nextView = View.atNow(currentView.viewStyle);
+      setCurrentView(nextView);
   };
 
   return (
@@ -244,29 +303,29 @@ function App() {
       <h1>Greenhouse Monitoring</h1>
 
       <div style={{ marginBottom: "16px" }}>
-        <button onClick={moveFastBackward}>FastBack</button>
-        <button onClick={moveBackward}>Back</button>
-        <button onClick={moveForward}>Forward</button>
-        <button onClick={moveFastForward}>Fast Forward</button>
+        <button onClick={moveFastBackwards}>FastBack</button>
+        <button onClick={moveBackwards}>Back</button>
+        <button onClick={moveForwards}>Forward</button>
+        <button onClick={moveFastForwards}>Fast Forward</button>
         <button onClick={setToNow}>Now</button>
       </div>
 
       <div style={{ marginBottom: "16px" }}>
         <button
-          onClick={() => setCurrentView(hourView)}
-          style={{ fontWeight: currentView === hourView ? "bold" : "normal" }}
+          onClick={() => setViewStyle(hourView)}
+          style={{ fontWeight: currentView.viewStyle === hourView ? "bold" : "normal" }}
         >
           Hour
         </button>
         <button
-          onClick={() => setCurrentView(dayView)}
-          style={{ fontWeight: currentView === dayView ? "bold" : "normal" }}
+          onClick={() => setViewStyle(dayView)}
+          style={{ fontWeight: currentView.viewStyle === dayView ? "bold" : "normal" }}
         >
           Day
         </button>
         <button
-          onClick={() => setCurrentView(weekView)}
-          style={{ fontWeight: currentView === weekView ? "bold" : "normal" }}
+          onClick={() => setViewStyle(weekView)}
+          style={{ fontWeight: currentView.viewStyle === weekView ? "bold" : "normal" }}
         >
           Week
         </button>
@@ -301,7 +360,7 @@ function App() {
               currentView.endTimestamp
             ]}
             tick={<CustomizedAxisTick data={data} />}
-            tickFormatter={currentView.tickFormatter}
+            tickFormatter={currentView.viewStyle.tickFormatter}
           />
           <YAxis />
           <Tooltip content={<CustomTooltip />} />
@@ -331,7 +390,7 @@ function App() {
         View starts at: {new Date(currentView.startTimestamp * 1000).toLocaleString()}
       </span>
       <span style={{ margin: "10px 15px" }}>
-        Now: {moment(Date.now()).format()}
+        Now: {moment(Date.now()).toLocaleString()}
       </span>
       <span style={{ margin: "10px 15px" }}>
         Selected sensor: {selectedSensor}
